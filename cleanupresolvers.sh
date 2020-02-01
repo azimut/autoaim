5799df1ca252115517cce0a6e0ae9942fd54a773
azimut <azimut.github@protonmail.com>
2020-01-31 22:08:32 -0300

    tmp, args cleanup

--- cleanupresolvers.sh
+++ cleanupresolvers.sh
@@ -2,16 +2,19 @@
 
 set -e
 
+JOBS=20
+FILE=${1:-$HOME/projects/sec/massdns/lists/resolvers.txt}
+
 # https://stackoverflow.com/questions/17307800/how-to-run-given-function-in-bash-in-parallel/17316302
 
 doit(){
     local resolverip="$1"
-    if ! ping -c1 ${resolverip} &>/dev/null; then
+    if ! ping -c1 "${resolverip}" &>/dev/null; then
         echo "DOWN ${resolverip} PING"
         return 1
     fi
 
-    if dig +timeout=1 +short google.com @${resolverip} &>/dev/null; then
+    if dig +timeout=1 +short google.com @"${resolverip}" &>/dev/null; then
         echo "UP ${resolverip}"
     else
         echo "DOWN ${resolverip} DNS"
@@ -19,5 +22,5 @@ doit(){
     return 0
 }
 export -f doit
-mapfile -t ips < $HOME/projects/sec/massdns/lists/resolvers.txt
-parallel -j10 doit ::: "${ips[@]}"
+mapfile -t ips < "${FILE}"
+parallel -j"${JOBS}" doit ::: "${ips[@]}"
