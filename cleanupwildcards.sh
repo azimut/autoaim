e1a541e458348d03ca3a5b7a33d0d9b1038bea50
azimut <azimut.github@protonmail.com>
2020-02-02 02:47:51 -0300

    fixup cleanup wildcards

--- cleanupwildcards.sh
+++ cleanupwildcards.sh
@@ -4,9 +4,9 @@ set -e
 
 AUTOAIM=$HOME/projects/sec/autoaim
 RESOLVERS="$AUTOAIM/resolvers.txt"
-PUBLIC="$AUTOAIM/public_suffix_list.dat"
+PUBLIC="$AUTOAIM/clean_public_suffix_list.dat"
 DOMAIN="$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 12 | head -n 1)"
-MASS=600
+MASS=${1:-100}
 
 uncomment(){ grep -v -e '^$' -e '^#' -e '^//' /dev/stdin; }
 prefix(){ sed 's#^#'"${1}"'#g' /dev/stdin; }
@@ -16,11 +16,11 @@ cd $HOME/projects/sec/massdns
 ./bin/massdns -t A \
               -o S \
               -w "${AUTOAIM}"/massdns_wildcard.txt \
-              -s $MASS \
+              -s "${MASS}" \
               -r "${RESOLVERS}" \
               <(cat "${PUBLIC}" | uncomment | fgrep -v '*' | prefix "${DOMAIN}.")
 
 fgrep -vxf \
-      <(grep $DOMAIN "${AUTOAIM}"/massdns_wildcard.txt | cut -f1 -d' ') \
+      <(grep $DOMAIN "${AUTOAIM}"/massdns_wildcard.txt | cut -f1 -d' ' | rev | cut -b2- | rev) \
       <(cat "${PUBLIC}" | uncomment | fgrep -v '*' | prefix "${DOMAIN}.") \
-    | cut -f2- -d. >  "${AUTOAIM}"/clean_public_suffix_list.dat
+      > "${AUTOAIM}"/clean_public_suffix_list.dat
