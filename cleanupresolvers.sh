7d9d265776dcdae51028a712df71207a22423ad2
azimut <azimut.github@protonmail.com>
2020-02-03 11:05:41 -0300

    add SOA check and fix sort...-nu assumes IPs are numbers

--- cleanupresolvers.sh
+++ cleanupresolvers.sh
@@ -10,20 +10,31 @@ FILE=${1:-$HOME/projects/sec/massdns/lists/resolvers.txt}
 
 doit(){
     local resolverip="$1"
+    local output
     if ! ping -n -c1 "${resolverip}" &>/dev/null; then
         echo "DOWN ${resolverip} PING"
         return 1
     fi
-
     if output=$(dig +short +timeout=1 PTR 8.8.8.8.in-addr.arpa @"${resolverip}"); then
-        if [[ $output == "dns.google." ]]; then
-            echo "UP ${resolverip}"
-        else
-            echo "DOWN ${resolverip} BOGUS"
+        if [[ $output != "dns.google." ]]; then
+            echo "DOWN ${resolverip} BOGUS_A"
             return 1
         fi
     else
-        echo "DOWN ${resolverip} DNS"
+        echo "DOWN ${resolverip} DNS_PTR"
+        return 1
+    fi
+    unset output
+    if output=$(dig +short +timeout=1 SOA google.com @"${resolverip}" | cut -f1 -d' '); then
+        if [[ $output == "ns1.google.com." ]]; then
+            echo "UP ${resolverip}"
+            return 0
+        else
+            echo "DOWN ${resolverip} BOGUS_SOA"
+            return 1
+        fi
+    else
+        echo "DOWN ${resolverip} DNS_SOA"
         return 1
     fi
     return 0
@@ -32,9 +43,9 @@ doit(){
 cd $HOME/projects/sec/autoaim/
 
 export -f doit
-mapfile -t ips < <(cat "${FILE}" | xargs | tr ' ' $'\n')
+mapfile -t ips < <(cat "${FILE}" | xargs | tr ' ' $'\n' | shuf)
 parallel -j"${JOBS}" doit ::: "${ips[@]}" \
     | tee raw_resolvers.txt
 grep UP raw_resolvers.txt \
     | cut -f2 -d' ' \
-    | sort -nu > resolvers.txt
+    | sort | uniq > resolvers.txt
