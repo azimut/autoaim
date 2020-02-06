7f480b72896ade4c4af83f92b0cf22893ae75cbe
azimut <azimut.github@protonmail.com>
2020-02-06 20:45:30 -0300

    scan: notify-send

--- scan.sh
+++ scan.sh
@@ -12,25 +12,25 @@ NMAP=/usr/bin/nmap
 while read -r ip ; do
     folder=data/"${ip/\//N}"
     mkdir -p "${folder}"
-    notify-send "Scanning ${ip}..."
+    notify-send -t 5000 "Scanning ${ip}..."
     # UDP
     if [[ ! -f ${folder}/udp.nmap ]]; then
         sudo $NMAP -sUV --packet-trace \
              -oA ${folder}/udp --max-retries=0 --reason -n -F -Pn ${ip}
         grep /open/ ${folder}/udp.gnmap &&
-            notify-send $(echo -e "At ${ip}\n$(egrep -o '[0-9]+/open/' ${folder}/udp.gnmap)")
+            notify-send -t 7000 "Open ports at ${ip}" "$(egrep -o '[0-9]+/open/' ${folder}/udp.gnmap)"
     fi
     # TCP
     if [[ ! -f ${folder}/tcp.nmap ]]; then
         sudo $NMAP -sT -v \
              -oA ${folder}/tcp --max-retries=0 --reason -n -F -Pn ${ip}
         grep /open/ ${folder}/tcp.gnmap &&
-            notify-send $(echo -e "At ${ip}\n$(egrep -o '[0-9]+/open/' ${folder}/tcp.gnmap)")
+            notify-send -t 7000 "Open ports at ${ip}" "$(egrep -o '[0-9]+/open/' ${folder}/tcp.gnmap)"
     fi
     # BING-IP2HOST
     if [[ ! -f ${folder}/bing-ip2hosts ]]; then
         bash $BING -u -o ${folder}/bing-ip2hosts ${ip}
         [[ -s ${folder}/bing-ip2hosts ]] &&
-            notify-send $(cat ${folder}/bing-ip2hosts)
+            notify-send -t 7000 "$(wc -l ${folder}/bing-ip2hosts) Bing domains found!" "$(cat ${folder}/bing-ip2hosts | head)"
     fi
 done < data/up.txt
