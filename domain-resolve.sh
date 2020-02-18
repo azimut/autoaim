e6f6b7756faad6cbaa32d0d86f453f447414044c
azimut <azimut.github@protonmail.com>
2020-02-18 16:59:47 -0300

    domain-resolve.sh: trusttrees, wildcard and servfail countermeasures

--- domain-resolve.sh
+++ domain-resolve.sh
@@ -1,67 +1,201 @@
 #!/bin/bash
 
-set -x
-set -e
-set -u
+set -exuo pipefail
 
-DOMAIN=${1}
+DOMAIN=${1:-${PWD##*/}}
 CONCURRENCY=${2:-20}
 
 RESOLVERS=$HOME/projects/sec/autoaim/resolvers.txt
 MASSDNS=$HOME/projects/sec/massdns/bin/massdns
+FOLDER=data/domains/resolved
 
-suffix(){ sed 's#$#'"${1}"'#g' /dev/stdin; }
+mkdir -p ${FOLDER}
+mkdir -p ${FOLDER}/trusttrees
+
+has_wildcard(){
+    local domain="${1}"
+    local ips=()
+    # NOTE: increase and add more resolvers if more ips are needed
+    for _ in {1..5}; do
+        random_sub=$(openssl rand -base64 32 | tr -dc 'a-z0-9' | fold -w16 | head -n1)
+        ips+=($(dig @1.1.1.1 +short "${random_sub}.${domain}"))
+    done
+    if [[ ${#ips[@]} -eq 0 ]]; then
+        return 1
+    fi
+    read -r -a unique_ips < <(printf '%s\n' "${ips[@]}" | sort -u)
+    printf '%s\n' "${unique_ips[@]}"
+    return 0
+}
+
+# TODO: only supports 1 IP to ignore for wildcard
+resolved_domains() {
+    local domain=${1}
+    local wildcard_ip=${2:-NADA}
+    local filename=a_${domain}.txt.gz
+    local filepath=${FOLDER}/${filename}
+    if [[ -f ${filepath} ]]; then
+        if [[ ${wildcard_ip} != "NADA" ]]; then
+            (
+                zgrep -A7 NOERROR ${filepath} \
+                    | grep -B3 -P '(IN A (?!'"${wildcard_ip}"')|IN CNAME )' \
+                    | grep 'IN A$' \
+                    | cut -f1 -d' ' \
+                    | sort \
+                    | uniq
+                echo ${domain}.
+            ) || true | sort -u
+        else
+            zgrep -A7 NOERROR ${filepath} \
+                | grep -B3 -E 'IN (A|CNAME) ' \
+                | grep 'IN A$' \
+                | cut -f1 -d' ' \
+                | sort \
+                | uniq
+        fi
+    fi
+}
+noerror_domains(){
+    local domain=${1}
+    local filename=a_${domain}.txt.gz
+    local filepath=${FOLDER}/${filename}
+    if [[ -f ${filepath} ]]; then
+        zgrep -A7 NOERROR ${filepath} \
+            | grep -B7 'IN SOA' \
+            | grep 'IN A' \
+            | cut -f1 -d' '
+    fi
+}
 grepdomain(){
-    egrep -h -o '[-[:alnum:]\.]+\.'${1} -r . \
+    grep -E -h -o '[-_[:alnum:]\.]+\.'${1} -r . \
         | sed 's/^32m//g' \
         | sed 's/^253A//g' \
         | sort | uniq
 }
-
+grepsubdomain(){
+    local domain=${1}
+    grepdomain ${domain} | sed 's/.'${domain}'$//g'
+}
+printfnumber(){
+    LC_NUMERIC=en_US printf "%'.f\n" "${1}"
+}
+graph_trusttrees(){
+    local domain=${1}
+    local filename=${domain}_trust_tree_graph.png
+    if [[ ! -f ${FOLDER}/trusttrees/${domain}_trusttrees.log ]]; then
+        cd ${FOLDER}/trusttrees
+        trusttrees --gandi-api-v5-key $GANDI_API \
+                   --resolvers <(echo -e "8.8.8.8\n1.1.1.1") \
+                   --target ${domain} -x png 2>&1 | tee ${domain}_trusttrees.log
+        mv output/${filename} .
+        rm -rf ./output
+        cd -
+    fi
+}
 massdns(){
     local type=${1}
     shift
-    local domains=($@)
+    local domains=("${@}")
+    local output=${FOLDER}/${type,,}_${DOMAIN}.txt
     $MASSDNS \
         -s ${CONCURRENCY} \
-        -o Sn \
+        --retry SERVFAIL \
+        -c 25 \
+        -o F \
         -t ${type} \
         -r ${RESOLVERS} \
-        -w data/resolved_${type,,}_${DOMAIN} \
-        <(printf '%s\n' ${domains[@]} | sort | uniq)
+        -w ${output} \
+        <(printf '%s\n' "${domains[@]}" | sort | uniq)
+    if [[ ${type} == 'A' ]]; then
+        if grep -q -e "IN ${type} " -e 'IN CNAME ' ${output}; then
+            grep -e "IN ${type} " -e 'IN CNAME ' ${output} \
+                 > ${FOLDER}/short_${type,,}_${DOMAIN}.txt
+        fi
+    else
+        if grep -q "IN ${type} " ${output}; then
+            grep "IN ${type} " ${output} \
+                 > ${FOLDER}/short_${type,,}_${DOMAIN}.txt
+        fi
+    fi
+    gzip --best -f ${output}
 }
-
-# > explode_domain www.google.com
-# www.google.com
-# www.google
-# www
 explode_domain(){
     local domain="${1}"
     local regex_dot='\.'
     echo ${domain}
     if [[ $domain =~ $regex_dot ]]; then
-        explode_domain "${domain%.*}"
+        explode_domain "${domain#*.}"
     fi
 }
-export -f explode_domain
+explode_domains(){
+    local domains=("${@}")
+    for domain in "${domains[@]}"; do
+        explode_domain "${domain}"
+    done
+}
+
+# Gave up right away if root domain returns SRVFAIL
+if dig @1.1.1.1 ${DOMAIN} A | grep SERVFAIL; then
+    echo "SERVFAIL returned for ${DOMAIN} giving up"
+    echo ${DOMAIN} > data/domains/servfail
+    exit 1
+fi
+if dig @1.1.1.1 $(openssl rand -base64 32 | tr -dc 'a-z0-9' | fold -w16 | head -n1).${DOMAIN} A | grep SERVFAIL; then
+    echo "SERVFAIL returned for ${DOMAIN} giving up"
+    echo ${DOMAIN} > data/domains/servfail_sub
+    exit 1
+fi
+
+# Wildcard detection
+mapfile -t wildcard_ips < <(has_wildcard ${DOMAIN})
+if [[ ${#wildcard_ips[@]} -gt 0 ]]; then
+    printf '%s\n' "${wildcard_ips[@]}" > data/domains/wilcards_${DOMAIN}
+fi
 
 # Adds subdomains found in the same "project"
-subdomains=($(grepdomain ${DOMAIN} | sed 's/.'${DOMAIN}'$//g')
-            $(cat ../*/data/sub*))
-subdomains=( $(printf '%s\n' ${subdomains[@]} | sort | uniq | xargs -n1 -I{} sh -c 'explode_domain {}' | sort | uniq) )
-domains=(${subdomains[@]/%/.${DOMAIN}})
+subdomains=($({ grepsubdomain ${DOMAIN}; cat ../*/data/sub*; } | sort | uniq))
+subdomains=($(explode_domains "${subdomains[@]}" | sort | uniq))
+printf '%s\n' "${subdomains[@]}" > ${FOLDER}/raw_subdomains_${DOMAIN}.txt
 
-massdns A    ${domains[@]}
-massdns AAAA ${domains[@]}
-massdns NS   ${domains[@]}
-massdns MX   ${domains[@]}
-massdns TXT  ${domains[@]}
+domains=("${subdomains[@]/%/.${DOMAIN}}")
+domains+=("${DOMAIN}")
 
-cat data/resolved_a_${DOMAIN} \
-    | grep ${DOMAIN%%.*} \
-    | grep ' A ' \
-    | cut -f3 -d' ' | sort | uniq | sort -n > data/ips.txt
+notify-send -t 10000 \
+            "Massdns A" \
+            "of $(printfnumber ${#domains[@]}) subdomains..."
 
-## mightbe.txt
-# CNAME of trail
-# SPF
+massdns A "${domains[@]}"
+mapfile -t domains < <(resolved_domains ${DOMAIN} "${wildcard_ips[@]}")
+
+if [[ ${#domains[@]} -gt 0 ]]; then
+    massdns AAAA "${domains[@]}"
+    massdns NS   "${domains[@]}"
+    massdns MX   "${domains[@]}"
+    massdns TXT  "${domains[@]}"
+fi
+# DNAME, SPF, DMARC, CNAME (i mean if it has it but also has other things)
+
+# TODO: CNAME domains are missing from IP gather
+grep -F -h ${DOMAIN} data/domains/*/short_a_${DOMAIN}.txt \
+    | grep -F 'IN A ' \
+    | cut -f5 -d' ' | sort | uniq | sort -V \
+    | tee data/ips.txt
+
+# CLI command:
+# fgrep -h starbucks.fr domains/*/short_a_starbucks.fr.txt | grep CNAME | cut -f1,5 -d' ' | sort | uniq | sort -k2,2d | column -t
+
+# TODO: CNAME domains are missing from trusttrees graph
+resolved_domains ${DOMAIN} "${wildcard_ips[@]}" |
+    while read -r nsdomain; do
+        graph_trusttrees "${nsdomain}"
+    done
+
+# Show Domains that NOERROR that could be bruteforced down
+rm -f data/domains/noerror
+for ndomain in $(noerror_domains ${DOMAIN}); do
+    if grep -q ${ndomain} <(resolved_domains ${DOMAIN} "${wildcard_ips[@]}"); then
+        continue
+    else
+        echo ${ndomain} | tee -a data/domains/noerror
+    fi
+done
