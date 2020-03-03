5e29157ccde717bb4af05a97d9fcd304412ea948
azimut <azimut.github@protonmail.com>
2020-03-03 05:09:09 -0300

    scan and up with new "ips" folders

--- scan.sh
+++ scan.sh
@@ -4,12 +4,7 @@ set -exuo pipefail
 
 NMAP_PARSE=$HOME/projects/sec/nmap-parse-output/nmap-parse-output
 BING=$HOME/projects/sec/bing-ip2hosts/bing-ip2hosts
-NMAP=/usr/bin/nmap
-
-# TODO: reenable tcp scanning on cloud
-
-# https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
-# https://ip-ranges.amazonaws.com/ip-ranges.json
+NMAP=/usr/local/bin/nmap
 
 # Assummes this in /etc/sudoers, where username is your username
 # username ALL = NOPASSWD: /usr/bin/nmap
@@ -19,80 +14,92 @@ uncomment(){
         | sed -e 's/#.*$//g' \
         | sed -e 's/;;.*$//g'
 }
-
 bingip2host(){
     local ip=${1}
-    local folder=data/${ip}
+    local folder=../ips/${ip}
     if [[ ! -f ${folder}/bing-ip2hosts ]]; then
         bash $BING -u -o ${folder}/bing-ip2hosts ${ip}
         if [[ -s ${folder}/bing-ip2hosts ]]; then
-            notify-send -t 10000 "$(wc -l ${folder}/bing-ip2hosts | cut -f1 -d' ') Bing domains found!" "$(cat ${folder}/bing-ip2hosts | head)"
+            notify-send -t 10000 "$(wc -l < ${folder}/bing-ip2hosts) Bing domains found!" "$(head ${folder}/bing-ip2hosts)"
         fi
     fi
 }
 nmap_tcp_fast(){
     local ip=${1}
-    local folder=data/${ip}
+    local folder=../ips/${ip}
     if [[ ! -f ${folder}/tcp.nmap ]]; then
+        notify-send -t 5000 "Scanning ${ip}..."
         sudo $NMAP -sT -v \
              -oA ${folder}/tcp --max-retries=0 --reason -n -F -Pn ${ip}
         if grep /open/ ${folder}/tcp.gnmap; then
-            notify-send -t 7000 "Open ports at ${ip}" "$(egrep -o '[0-9]+/open/' ${folder}/tcp.gnmap)"
+            notify-send -t 7000 "Open ports at ${ip}" "$(grep -E -o '[0-9]+/open/' ${folder}/tcp.gnmap)"
         fi
     fi
 }
 nmap_udp_20(){
     local ip=${1}
-    local folder=data/${ip}
+    local folder=../ips/${ip}
     if [[ ! -f ${folder}/udp.nmap ]]; then
-        sudo $NMAP -sUV --packet-trace \
+        notify-send -t 5000 "Scanning ${ip}..."
+        sudo $NMAP -sUVC --packet-trace \
              --top-ports=20 \
              -oA ${folder}/udp --max-retries=0 --reason -n -F -Pn ${ip}
         if grep /open/ ${folder}/udp.gnmap; then
-            notify-send -t 7000 "Open ports at ${ip}" "$(egrep -o '[0-9]+/open/' ${folder}/udp.gnmap)"
+            notify-send -t 7000 "Open ports at ${ip}" "$(grep -E -o '[0-9]+/open/' ${folder}/udp.gnmap)"
         fi
     fi
 }
 nmap_tcp_full(){
     local ip=${1}
-    local folder=data/${ip}
+    local folder=../ips/${ip}
     if [[ ! -f ${folder}/full_tcp.nmap ]]; then
+        notify-send -t 5000 "FULL TCP scanning ${ip}..."
         sudo $NMAP -sT -v \
-             -oA ${folder}/full_tcp --max-retries=0 --reason -n -p- -Pn ${ip}
+             -oA ${folder}/full_tcp --max-retries=0 \
+             --reason -n -p- -Pn ${ip}
         if grep /open/ ${folder}/full_tcp.gnmap; then
-            notify-send -t 7000 "Open ports at ${ip}" "$(egrep -o '[0-9]+/open/' ${folder}/full_tcp.gnmap)"
+            notify-send -t 7000 "Open ports at ${ip}" "$(grep -E -o '[0-9]+/open/' ${folder}/full_tcp.gnmap)"
         fi
     fi
 }
+nmap_ext(){
+    local nmap_ext=( ssh ssl smtp pop3 tls imap )
+    local nmap_string=""
+    for proto in "${nmap_ext[@]}"; do
+        nmap_string+=" or (*${proto}* and (discovery or safe or auth))"
+    done
+    echo "${nmap_string}"
+}
 nmap_tcp_version(){
     local ip=${1}
-    local output=data/${ip}/full_tcp_version
-    local input=data/${ip}/full_tcp.xml
-    local ports="$(bash ${NMAP_PARSE} ${input} ports)"
-    if [[ ! -f ${output}.nmap && -f ${input} && -n ${ports} ]]; then
-        sudo ${NMAP} -sTVC -v \
+    local output=../ips/${ip}/full_tcp_version
+    local input=../ips/${ip}/full_tcp.xml
+    if [[ ! -f ${input} ]]; then
+        return 1
+    fi
+    local ports; ports="$(bash ${NMAP_PARSE} ${input} ports)"
+    if [[ ! -f ${output}.nmap && -n ${ports} ]]; then
+        notify-send -t 10000 "FULL Version ${ip}..."
+        sudo ${NMAP} -sTV --script='default or banner or unusual-port'"$(nmap_ext)" -v \
              -oA ${output} \
              --reason -n \
              -p${ports} \
              -Pn ${ip}
     fi
 }
-
-while read -r ip ; do
-    folder=data/${ip}
-    mkdir -p "${folder}"
-    notify-send -t 5000 "Scanning ${ip}..."
-    nmap_udp_20   ${ip}
-    nmap_tcp_fast ${ip}
-    bingip2host   ${ip}
-done < <(fgrep -v -e "CLOUDFRONT" -e "LOCAL" -e "Akamai" -e "AzureFrontDoor.Frontend" -e "Cloudflare" data/up.txt)
-
-if [[ -s data/full.txt ]]; then
+if [[ -s data/ips.txt ]]; then
     while read -r ip ; do
-        folder=data/${ip}
-        mkdir -p "${folder}"
-        notify-send -t 5000 "FULL Scanning ${ip}..."
-        nmap_tcp_full    ${ip}
-        nmap_tcp_version ${ip}
-    done < <(cat data/full.txt | uncomment | trim)
+        if [[ -f ../ips/${ip}/up ]]; then
+            nmap_udp_20   ${ip}
+            nmap_tcp_fast ${ip}
+            bingip2host   ${ip}
+        fi
+    done < data/ips.txt
+    while read -r ip ; do
+        if [[ -f ../ips/${ip}/full ]]; then
+            nmap_tcp_full    ${ip}
+            nmap_tcp_version ${ip}
+        fi
+    done < data/ips.txt
 fi
+
--- up.sh
+++ up.sh
@@ -2,6 +2,39 @@
 
 set -exuo pipefail
 
+mkdir -p data/
+
+#BYPASS=$HOME/projects/sec/bypass-firewalls-by-DNS-history/bypass-firewalls-by-DNS-history.sh
+NMAP=/usr/bin/nmap
+DATE=$(date +%s)
+UA="http.useragent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'"
+# hostmap- scripts are broken
+SCRIPTS=(
+    address-info
+    dns-blacklist
+    dns-zeustracker
+    fcrdns
+    traceroute-geolocation
+    whois-ip
+)
+nmap_alive(){
+    local ip=${1}
+    local filename=../ips/${ip}/alive${DATE}
+    sudo $NMAP -n \
+         -sn \
+         -PE -PS80,443 -PA80 -PP \
+         -oA ${filename} \
+         --script-args="${UA},traceroute-geolocation.kmlfile='../ips/${ip}/map'" \
+         --traceroute \
+         --script "$(printf '%s,' "${SCRIPTS[@]}")" \
+         --reason -v \
+         ${ip}
+    if grep Up ${filename}.gnmap; then
+        touch ../ips/${ip}/up
+    else
+        touch ../ips/${ip}/down
+    fi
+}
 trim(){ awk '{$1=$1};1' /dev/stdin; }
 uncomment(){
     grep -v -e '^$' -e '^#' -e '^//' -e '^;;' /dev/stdin \
@@ -14,62 +47,58 @@ grepip(){
 ips_without_provider(){
     uncomment < data/ips.txt \
         | trim \
-        | grep -vxf <(find data/ -name provider | grepip)
+        | grep -vxf <(find ../ips/ -type f -name provider | grepip)
 }
-#BYPASS=$HOME/projects/sec/bypass-firewalls-by-DNS-history/bypass-firewalls-by-DNS-history.sh
-NMAP=/usr/bin/nmap
-DATE=$(date +%s)
-UA="http.useragent='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0'"
-# hostmap- scripts are broken
-SCRIPTS=(
-    asn-query
-    address-info
-    dns-blacklist
-    dns-zeustracker
-    fcrdns
-    traceroute-geolocation
-    whois-ip
-)
+upsert_in_file(){
+    local file="${1}"
+    shift
+    local inserts=("${@}")
+    [[ ! -f ${file} ]] && return 1
+    for insert in "${inserts[@]}" ; do
+        grep -F -x "${insert}" "${file}" \
+            || echo "${insert}" >> "${file}"
+    done
+}
 
-mkdir -p data/
+# If no ips in domain, quit
+[[ ! -s  data/ips.txt ]] && { exit 1; }
 
-if compgen -G 'data/alive*.gnmap'; then
-    grep Up   data/alive*.gnmap | cut -f2 -d' ' | sort | uniq | sort -V > data/up.txt   || true
-    grep Down data/alive*.gnmap | cut -f2 -d' ' | sort | uniq | sort -V > data/down.txt || true
-    cat data/up.txt data/down.txt | sort | uniq | sort -V > data/processed.txt
-else
-    touch data/processed.txt
-fi
+# Add domain into ips list
+uncomment < data/ips.txt | trim |
+    while read -r ip; do
+        domain=${PWD%/}
+        domain=${domain##*/}
+        mkdir -p ../ips/${ip}/
+        touch ../ips/${ip}/domains
+        upsert_in_file ../ips/${ip}/domains ${domain}
+    done
 
-# Note: Plus -PS80 from default
-sudo $NMAP -n \
-     -sn \
-     -PE -PS80,443 -PA80 -PP \
-     -oA data/alive${DATE} \
-     --script-args="${UA}" \
-     --traceroute \
-     --script "$(printf '%s,' "${SCRIPTS[@]}")" \
-     --reason -v \
-     --excludefile data/processed.txt \
-     --randomize-hosts \
-     -iL data/ips.txt
+# nmap ping check
+uncomment < data/ips.txt | trim |
+    while read -r ip; do
+        if [[ ! -f ../ips/${ip}/up && ! -f ../ips/${ip}/down ]]; then
+            nmap_alive ${ip}
+        fi
+    done
 
-grep Up   data/alive*.gnmap | cut -f2 -d' ' | sort | uniq | sort -V > data/up.txt   || true
-grep Down data/alive*.gnmap | cut -f2 -d' ' | sort | uniq | sort -V > data/down.txt || true
-cat data/up.txt data/down.txt | sort | uniq | sort -V > data/processed.txt
+# Add PTR
+uncomment < data/ips.txt | trim |
+    while read -r ip; do
+        if [[ ! -f ../ips/${ip}/ptr ]]; then
+            dig +short @1.1.1.1 -x ${ip} > ../ips/${ip}/ptr
+        fi
+    done
 
 # Add Provider - Ignore ips already processed
 mapfile -t pending < <(ips_without_provider)
-
 if [[ ${#pending[@]} -ne 0 ]]; then
     printf "%s\n" "${pending[@]}" | sunny |
         while IFS=, read -r ip cidr provider; do
-            mkdir -p data/${ip}
             cidr=${cidr// }
             provider=${provider// }
             provider=${provider//\"}
-            echo ${cidr}     > data/${ip}/cidr
-            echo ${provider} > data/${ip}/provider
+            echo ${cidr}     > ../ips/${ip}/cidr
+            echo ${provider} > ../ips/${ip}/provider
         done
 fi
 ## TODO: I can't do this properly without follow CNAME's resolved, I mean...I want to target
@@ -80,12 +109,3 @@ fi
 #         bash ${BYPASS} 2>&1 | tee data/${ip}/bypass
 #     fi
 # fi
-
-# Add PTR
-uncomment < data/ips.txt | trim |
-    while read -r ip; do
-        mkdir -p data/${ip}
-        if [[ ! -f data/${ip}/ptr ]]; then
-            dig +short @1.1.1.1 -x ${ip} > data/${ip}/ptr
-        fi
-    done
