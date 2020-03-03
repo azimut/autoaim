0d9fc16e2ad1aaeeeee55e54be300e556c0a56b2
azimut <azimut.github@protonmail.com>
2020-03-03 05:13:08 -0300

    comment out domain for now

--- monit.sh
+++ monit.sh
@@ -2,17 +2,57 @@
 
 set -exuo pipefail
 
+DIRS=(${@:-.})
 OUTPUT_FILE=bounty.conf
 BASE_SERVERS=(8.8.8.8 1.1.1.1 9.9.9.9)
+INTERVAL_WIDTH_PING=60
+INTERVAL_BASE_PING=60
 
+for dir in "${DIRS[@]}"; do
+    [[ ! -d ${dir} ]] && { echo "NOT A DIR!"; exit 1; }
+done
+
+grepip(){
+    grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" /dev/stdin
+}
 trim(){ awk '{$1=$1};1' /dev/stdin; }
 uncomment(){
     grep -v -e '^$' -e '^#' -e '^//' -e '^;;' /dev/stdin \
         | sed -e 's/#.*$//g' \
         | sed -e 's/;;.*$//g'
 }
+resolved_domains() {
+    local domain=${1}
+    shift
+    local wildcard_ips=("${@}")
+    local filename=a_${domain}.txt.gz
+    local filepath=${FOLDER}/${filename}
+    if [[ -f ${filepath} ]]; then
+        if [[ ${#wildcard_ips[@]} -ne 0 ]]; then
+            (
+                wildcard_grep="${wildcard_ips[*]/%/|}"
+                wildcard_grep="${wildcard_grep:0:-1}"
+                wildcard_grep="${wildcard_grep// /}"
+                zgrep -A7 NOERROR ${filepath} \
+                    | grep -B3 -P '(IN A (?!'"${wildcard_grep}"')|IN CNAME )' \
+                    | grep 'IN A$' \
+                    | cut -f1 -d' ' \
+                    | sort \
+                    | uniq
+                echo ${domain}.
+            ) || true | sort -u
+        else
+            zgrep -A7 NOERROR ${filepath} \
+                | grep -B3 -E 'IN (A|CNAME) ' \
+                | grep 'IN A$' \
+                | cut -f1 -d' ' \
+                | sort \
+                | uniq
+        fi
+    fi
+}
 grepdomain(){
-    egrep -I -h -o '[-_[:alnum:]\.]+\.'${1} -r . \
+    grep -E -I -h -o '[-_[:alnum:]\.]+\.'${1} -r . \
         | sed 's/^2F//g' \
         | sed 's/^32m//g' \
         | sed 's/^253A//g' \
@@ -23,7 +63,7 @@ complement(){
     [[ (-f $1 || -p $1) && (-f $2 || -p $2) ]] || { ferror "arguments need to be a file"; return 1; }
     local filea="$1"
     local fileb="$2"
-    fgrep -vxf "$filea" "$fileb"
+    grep -F -vxf "$filea" "$fileb"
 }
 
 cat > ${OUTPUT_FILE} <<EOF
@@ -36,33 +76,34 @@ EOF
 #      Assumes some dir structure...
 # (scdr|admin.login).starbucks.co.jp returns NOERROR not NXDOMAIN???
 # test.istarbucks.co.kr NOERROR due one of the NS servers is missing the record
-find *.*/ -type d -name resolved |
-    while read -r rdir; do
-        domain=${rdir%/data*}
-        domain=${domain#*/}
-        cd ${rdir}
-        unresolved_domains=(
-            $(complement <(grepdomain ${domain}) \
-                         <(cd ..; grepdomain ${domain} | grep -v -e scdr.starbucks.co.jp -e admin.login.starbucks.co.jp -e test.istarbucks.co.kr; ) | sort | uniq)
-        )
-        cd -
-        for udomain in ${unresolved_domains[@]}; do
-            cat >> ${OUTPUT_FILE} <<EOF
-[[inputs.dns_query]]
-  interval = "$((40 + $RANDOM % 60))m"
-  servers     = ["${BASE_SERVERS[$((RANDOM % 3))]}"]
-  record_type = "A"
-  domains     = ["${udomain}"]
-EOF
-        done
-    done
+# rekindle.starbucks.ca NOERROR due has only NS defined
+# find *.*/ -type d -name resolved |
+#     while read -r rdir; do
+#         domain=${rdir%/data*}
+#         domain=${domain#*/}
+#         cd ${rdir}
+#         unresolved_domains=(
+#             $(complement <(resolved_domains ${domain}) \
+#                          <(cd ..; grepdomain ${domain} | grep -F -v -e scdr.starbucks.co.jp -e admin.login.starbucks.co.jp -e test.istarbucks.co.kr -e rekindle.starbucks.ca; ) | sort | uniq)
+#         )
+#         cd -
+#         for udomain in "${unresolved_domains[@]}"; do
+#             cat >> ${OUTPUT_FILE} <<EOF
+# [[inputs.dns_query]]
+#   interval = "$((40 + $RANDOM % 60))m"
+#   servers     = ["${BASE_SERVERS[$((RANDOM % 3))]}"]
+#   record_type = "A"
+#   domains     = ["${udomain}"]
+# EOF
+#         done
+#     done
 
 # IP: Adds all down ips
-find *.*/ -type f -name down.txt -exec cat {} \; | uncomment | trim | sort | uniq |
+find "${DIRS[@]}" -type f -name down | grepip | sort -V |
     while read -r ip; do
         cat >> ${OUTPUT_FILE} <<EOF
 [[inputs.ping]]
-  interval = "$((40 + $RANDOM % 60))m"
+  interval = "$((INTERVAL_BASE_PING + RANDOM % INTERVAL_WIDTH_PING))m"
   urls     = ["${ip}"]
   count    = 1
   method   = "native"
