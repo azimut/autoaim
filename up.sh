463cd597f63fe2b3edfc09c02aa8c22c68d45969
azimut <azimut.github@protonmail.com>
2020-06-22 10:10:02 -0300

    up: adding persistence usage, use helpers

--- up.sh
+++ up.sh
@@ -2,8 +2,6 @@
 
 set -exuo pipefail
 
-mkdir -p data/
-
 #BYPASS=$HOME/projects/sec/bypass-firewalls-by-DNS-history/bypass-firewalls-by-DNS-history.sh
 NMAP=/usr/bin/nmap
 DATE=$(date +%s)
@@ -17,6 +15,10 @@ SCRIPTS=(
     traceroute-geolocation
     whois-ip
 )
+
+. ${HOME}/projects/sec/autoaim/helpers.sh
+. ${HOME}/projects/sec/autoaim/persistence.sh
+
 nmap_alive(){
     local ip=${1}
     local filename=../ips/${ip}/alive${DATE}
@@ -30,41 +32,28 @@ nmap_alive(){
          --reason -v \
          ${ip}
     if grep Up ${filename}.gnmap; then
-        touch ../ips/${ip}/up
+        touch ../ips/${ip}/up  ; echo ${ip} | add_ips_up
     else
-        touch ../ips/${ip}/down
+        touch ../ips/${ip}/down; echo ${ip} | add_ips_down
     fi
 }
-trim(){ awk '{$1=$1};1' /dev/stdin; }
-uncomment(){
-    grep -v -e '^$' -e '^#' -e '^//' -e '^;;' /dev/stdin \
-        | sed -e 's/#.*$//g' \
-        | sed -e 's/;;.*$//g'
+ips_up(){
+    find .. -type f -name up | cut -f3 -d/
 }
-grepip(){
-    grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" /dev/stdin
+ips_down(){
+    find .. -type f -name down | cut -f3 -d/
 }
 ips_without_provider(){
-    uncomment < data/ips.txt \
+    uncomment < ips.txt \
         | trim \
         | grep -vxf <(find ../ips/ -type f -name provider | grepip)
 }
-upsert_in_file(){
-    local file="${1}"
-    shift
-    local inserts=("${@}")
-    [[ ! -f ${file} ]] && return 1
-    for insert in "${inserts[@]}" ; do
-        grep -F -x "${insert}" "${file}" \
-            || echo "${insert}" >> "${file}"
-    done
-}
 
 # If no ips in domain, quit
-[[ ! -s  data/ips.txt ]] && { exit 1; }
+[[ ! -s  ips.txt ]] && { exit 1; }
 
 # Add domain into ips list
-uncomment < data/ips.txt | trim |
+uncomment < ips.txt | trim |
     while read -r ip; do
         domain=${PWD%/}
         domain=${domain##*/}
@@ -74,7 +63,7 @@ uncomment < data/ips.txt | trim |
     done
 
 # nmap ping check
-uncomment < data/ips.txt | trim |
+uncomment < ips.txt | trim |
     while read -r ip; do
         if [[ ! -f ../ips/${ip}/up && ! -f ../ips/${ip}/down ]]; then
             nmap_alive ${ip}
@@ -82,7 +71,7 @@ uncomment < data/ips.txt | trim |
     done
 
 # Add PTR
-uncomment < data/ips.txt | trim |
+uncomment < ips.txt | trim |
     while read -r ip; do
         if [[ ! -f ../ips/${ip}/ptr ]]; then
             dig +short @1.1.1.1 -x ${ip} > ../ips/${ip}/ptr
@@ -105,7 +94,7 @@ fi
 ##       only domains behind a cloud provider...a way could it be provide more ips to bypass
 # Bypass
 # if [[ ${provider} == "CLOUDFRONT" || ${provider} == "Akamai" || ${provider} == "AzureFrontDoor.Frontend" || ${provider} == "Cloudflare" ]]; then
-#     if [[ ! -f data/${ip}/bypass ]]; then
-#         bash ${BYPASS} 2>&1 | tee data/${ip}/bypass
+#     if [[ ! -f ${ip}/bypass ]]; then
+#         bash ${BYPASS} 2>&1 | tee ${ip}/bypass
 #     fi
 # fi
