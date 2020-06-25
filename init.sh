eceeaa972112696de3f4c06650fcd2a7a8c8673a
azimut <azimut.github@protonmail.com>
2020-06-25 20:13:30 -0300

    init send domain

--- init.sh
+++ init.sh
@@ -22,11 +22,11 @@ uncomment < ${FILE} | trim |
         cd ${domain}
         in_array domain          "${TASKS[@]}" && bash ${AUTOAIM}/domain.sh          ${domain}
         in_array resolve         "${TASKS[@]}" && bash ${AUTOAIM}/domain-resolve.sh  ${domain}
-        in_array mx              "${TASKS[@]}" && bash ${AUTOAIM}/mx.sh
-        in_array ns              "${TASKS[@]}" && bash ${AUTOAIM}/ns.sh
+        in_array mx              "${TASKS[@]}" && bash ${AUTOAIM}/mx.sh              ${domain}
+        in_array ns              "${TASKS[@]}" && bash ${AUTOAIM}/ns.sh              ${domain}
         in_array takeover        "${TASKS[@]}" && bash ${AUTOAIM}/takeover.sh        ${domain}
         in_array second-takeover "${TASKS[@]}" && bash ${AUTOAIM}/second-takeover.sh ${domain}
-        in_array up              "${TASKS[@]}" && bash ${AUTOAIM}/up.sh
+        in_array up              "${TASKS[@]}" && bash ${AUTOAIM}/up.sh              ${domain}
         in_array scan            "${TASKS[@]}" && bash ${AUTOAIM}/scan.sh
         in_array screenshot      "${TASKS[@]}" && bash ${AUTOAIM}/screenshot.sh
         cd - &>/dev/null
