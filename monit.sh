226e8bfa6296af965e90081a2f0647c510b1abea
azimut <azimut.github@protonmail.com>
2020-06-26 23:59:07 -0300

    monit with persistence

--- monit.sh
+++ monit.sh
@@ -2,69 +2,10 @@
 
 set -exuo pipefail
 
-DIRS=(${@:-.})
 OUTPUT_FILE=bounty.conf
 BASE_SERVERS=(8.8.8.8 1.1.1.1 9.9.9.9)
 INTERVAL_WIDTH_PING=60
 INTERVAL_BASE_PING=60
-
-for dir in "${DIRS[@]}"; do
-    [[ ! -d ${dir} ]] && { echo "NOT A DIR!"; exit 1; }
-done
-
-grepip(){
-    grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" /dev/stdin
-}
-trim(){ awk '{$1=$1};1' /dev/stdin; }
-uncomment(){
-    grep -v -e '^$' -e '^#' -e '^//' -e '^;;' /dev/stdin \
-        | sed -e 's/#.*$//g' \
-        | sed -e 's/;;.*$//g'
-}
-resolved_domains() {
-    local domain=${1}
-    shift
-    local wildcard_ips=("${@}")
-    local filename=a_${domain}.txt.gz
-    local filepath=${FOLDER}/${filename}
-    if [[ -f ${filepath} ]]; then
-        if [[ ${#wildcard_ips[@]} -ne 0 ]]; then
-            (
-                wildcard_grep="${wildcard_ips[*]/%/|}"
-                wildcard_grep="${wildcard_grep:0:-1}"
-                wildcard_grep="${wildcard_grep// /}"
-                zgrep -A7 NOERROR ${filepath} \
-                    | grep -B3 -P '(IN A (?!'"${wildcard_grep}"')|IN CNAME )' \
-                    | grep 'IN A$' \
-                    | cut -f1 -d' ' \
-                    | sort \
-                    | uniq
-                echo ${domain}.
-            ) || true | sort -u
-        else
-            zgrep -A7 NOERROR ${filepath} \
-                | grep -B3 -E 'IN (A|CNAME) ' \
-                | grep 'IN A$' \
-                | cut -f1 -d' ' \
-                | sort \
-                | uniq
-        fi
-    fi
-}
-grepdomain(){
-    grep -E -I -h -o '[-_[:alnum:]\.]+\.'${1} -r . \
-        | sed 's/^2F//g' \
-        | sed 's/^32m//g' \
-        | sed 's/^253A//g' \
-        | sort | uniq
-}
-complement(){
-    [[ $# -ne 2 ]] && { ferror "needs 2 arguments"; return 1; }
-    [[ (-f $1 || -p $1) && (-f $2 || -p $2) ]] || { ferror "arguments need to be a file"; return 1; }
-    local filea="$1"
-    local fileb="$2"
-    grep -F -vxf "$filea" "$fileb"
-}
 
 cat > ${OUTPUT_FILE} <<EOF
 [agent]
@@ -99,7 +40,7 @@ EOF
 #     done
 
 # IP: Adds all down ips
-find "${DIRS[@]}" -type f -name down | grepip | sort -V |
+get_all_down_ips |
     while read -r ip; do
         cat >> ${OUTPUT_FILE} <<EOF
 [[inputs.ping]]
@@ -127,8 +68,8 @@ cat >> ${OUTPUT_FILE} <<EOF
 
 # PING
 [[outputs.exec]]
-  command = ["xargs","-n1","-I{}","notify-send","--urgency=critical","dead IP is back online","{}"]
-  namepass = ["ping"]
+  command   = ["xargs","-n1","-I{}","notify-send","--urgency=critical","dead IP is back online","{}"]
+  namepass  = ["ping"]
   fieldpass = ["ttl"]
 
 [[outputs.file]]
