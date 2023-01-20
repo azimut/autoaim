59197e8d6c2258bf1d27d4b430a8a17d3cddb4d8
azimut <azimut.github@protonmail.com>
2023-01-20 01:14:42 -0300

    mx.sh: minor refactor

--- mx.sh
+++ mx.sh
@@ -4,19 +4,22 @@ set -exuo pipefail
 
 DOMAIN=${1:-${PWD##*/}}
 
+BLACKLISTED_MX_DOMAINS='(mailgun.org|google.com|googlemail.com)'
+
 [[ -f ../env.sh ]] && source ../env.sh
 . ${HOME}/projects/sec/autoaim/helpers.sh
 . ${HOME}/projects/sec/autoaim/persistence.sh
 
-nmap_ext() {
-	local nmap_ext=(ssh ssl smtp pop3 tls imap)
-	local nmap_string=""
-	for proto in "${nmap_ext[@]}"; do
-		nmap_string+=" or (*${proto}* and (discovery or safe or auth))"
+nmap_scripts() {
+	local protocols=(ssh ssl smtp pop3 tls imap)
+	local output=""
+	for protocol in "${protocols[@]}"; do
+		output+=" or (*${protocol}* and (discovery or safe or auth))"
 	done
-	echo "${nmap_string}"
+	echo "${output}"
 }
 
+# BUG: nmap needs separate executions for ipv4/ipv6
 nmap_mx() {
 	local mx=${1}
 	local file
@@ -31,7 +34,7 @@ nmap_mx() {
 			-6 \
 			--script-args="http.useragent='${UA}'" \
 			--resolve-all \
-			--script='default or banner or fcrdns'"$(nmap_ext)" \
+			--script='default or banner or fcrdns'"$(nmap_scripts)" \
 			${mx}
 	fi
 	add_scan_file ${file}
@@ -44,19 +47,17 @@ nmap_mx() {
 			-oA ${file} \
 			--script-args="http.useragent='${UA}'" \
 			--resolve-all \
-			--script='default or banner or fcrdns'"$(nmap_ext)" \
+			--script='default or banner or fcrdns'"$(nmap_scripts)" \
 			${mx}
 	fi
 	add_scan_file ${file}
 }
 
-# add ALL, mx dns records
 for qtype in 'A' 'AAAA'; do
 	dns_mx "${DOMAIN}" | cut -f2 -d'|' | sort -u | massdns_inline ${qtype} | add_other ${qtype}
 done
 
-# scan mx domains
-dns_mx "${DOMAIN}" | cut -f2 -d'|' | sort -u | grep -F -v -e 'mailgun.org' -e 'google.com' -e 'googlemail.com' |
+dns_mx ${DOMAIN} | cut -f2 -d'|' | sort -u | grep -E -v -e "${BLACKLISTED_MX_DOMAINS}" |
 	while read -r mx; do
 		nmap_mx ${mx}
 	done
